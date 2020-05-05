b9a40e49b37a4c5a6b810b245b2d0b6c080e018b
Audrey <audrey-lopes-correia@hotmail.fr>
2020-05-05 15:08:31 +0200

    basic auth - Test OK

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,21 +1,21 @@
 const crypto = require('crypto');
 
 function sha1Encode(data) {
-    const encoded = data.replace('Basic','');
-    return encoded;
+    const sha1Encode = crypto.createHash('sha1');
+    sha1Encode.update(data);
+    return sha1Encode.digest('hex');
 }
 
 module.exports.digestAuth = (request, response, next) => {
-    const authorization = request.headers.authorization; //Basic xxxxx
-    console.log('authorization', authorization)
+    const authorization = request.headers.authorization;
 
-    const encoded = sha1Encode(authorization);
-    const decoded = Buffer.from(encoded, crypto).toString("utf8");
+    const encoded = authorization.replace('Basic ', '');
+    const decoded = Buffer.from(encoded, 'base64').toString("utf8");
     
     const authentification = decoded.split(':');
 
-    //si user =user et password = password , ok
-    const isValid = authentification[0] === 'node' && authentification[1] === 'password';
+    const isValid = authentification[0] === 'node' && authentification[1] === sha1Encode('password');
     
-    isValid ? next() : response.sendStatus(401);
+    const HTTP_CODE_UNAUTHORIZED = 401; 
+    isValid ? next() : response.sendStatus(HTTP_CODE_UNAUTHORIZED);
 }
