f8ed55357cdb2202c8535fba09ebe955cc228965
Audrey <audrey-lopes-correia@hotmail.fr>
2020-05-05 14:51:38 +0200

    basic auth

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,21 @@
 const crypto = require('crypto');
 
 function sha1Encode(data) {
-    // To be implemented!
+    const encoded = data.replace('Basic','');
+    return encoded;
 }
 
 module.exports.digestAuth = (request, response, next) => {
-    // To be implemented!
+    const authorization = request.headers.authorization; //Basic xxxxx
+    console.log('authorization', authorization)
+
+    const encoded = sha1Encode(authorization);
+    const decoded = Buffer.from(encoded, crypto).toString("utf8");
+    
+    const authentification = decoded.split(':');
+
+    //si user =user et password = password , ok
+    const isValid = authentification[0] === 'node' && authentification[1] === 'password';
+    
+    isValid ? next() : response.sendStatus(401);
 }
